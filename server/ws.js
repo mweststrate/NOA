b7b8ed628dbf8ea267c9a4481c5eacec840611cd
Michel Weststrate <mweststrate@gmail.com>
2013-02-24 19:57:02 +0100

    fixed compile errors

--- server/ws.js
+++ server/ws.js
@@ -1,5 +1,5 @@
 var io = require('socket.io')
-var core = require('./server/core.js')
+var core = require('./core.js')
 
 var clientcount = 0;
 var clients = {};
@@ -7,9 +7,7 @@ var clients = {};
 
 
 exports.start = function(app) {
-	io.listen(app);
-
-	io.sockets.on('connection', onConnect)
+	io.listen(app).sockets.on('connection', onConnect)
 
 }
 
@@ -17,21 +15,19 @@ function onConnect(socket) {
 	var socketIdx = ++clientcount;
 	var socketData = {	}; //bound to scope of socket
 
-	socket.set('data', socketData, function() {
-		clients[socketIdx] = socket;
-	
-		socket.on('disconnect', function() {
-			delete clients[socketIdx]
-		}
+	clients[socketIdx] = socket;
 
-		io.sockets.on('put', function(data, cb) {
-			core.update(data, cb); //cb is received on client!
-		})
+	socket.on('disconnect', function() {
+		delete clients[socketIdx]
+	})
 
-		io.sockets.on('get', function(id, cb) {
-			core.getById(id, cb);
-		})
-	}
+	io.sockets.on('put', function(data, cb) {
+		core.update(data, cb); //cb is received on client!
+	})
+
+	io.sockets.on('get', function(id, cb) {
+		core.getById(id, cb);
+	})
 }
 
 function onDisconnect(socket) {
