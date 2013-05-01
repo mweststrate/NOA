df5ecf40537ac0fdd094e8fb7e0a1b957cc93d13
Michel Weststrate <mweststrate@gmail.com>
2013-05-01 07:46:58 +0200

    whitespace to tabs

--- src/record.ts
+++ src/record.ts
@@ -2,74 +2,74 @@
 
 module NOA{
 
-    export class Record extends CellContainer implements IValue {
+	export class Record extends CellContainer implements IValue {
 
-        data = {};
-        keys = new List();
+		data = {};
+		keys = new List();
 
-        set (key : string, value : any) {
-            if (!this.has(key)) {
-                this.data[key] = new Cell(this, key, value, this);
-                this.keys.add(key, this);
-                this.fire('set',key, value, undefined); //todo, needs to fire or is done automatically?
-            }
+		set (key : string, value : any) {
+			if (!this.has(key)) {
+				this.data[key] = new Cell(this, key, value, this);
+				this.keys.add(key, this);
+				this.fire('set',key, value, undefined); //todo, needs to fire or is done automatically?
+			}
 
-            else if (this.get(key) != value) {
-                (<Cell>this.data[key]).set(value); //fires event
-            }
-        }
+			else if (this.get(key) != value) {
+				(<Cell>this.data[key]).set(value); //fires event
+			}
+		}
 
-        fireCellChanged(index: any, newvalue: any, oldvalue: any) {
-            this.fire('set', index, newvalue, oldvalue);
-        };
+		fireCellChanged(index: any, newvalue: any, oldvalue: any) {
+			this.fire('set', index, newvalue, oldvalue);
+		};
 
-        remove(key : string) {
-            if (!this.has(key))
-                return;
+		remove(key : string) {
+			if (!this.has(key))
+				return;
 
-            this.fire('remove', key);
-            (<Cell>this.data[key]).free();
+			this.fire('remove', key);
+			(<Cell>this.data[key]).free();
 
-            this.keys.removeAll(key);
-            delete this.data[key];
-        }
+			this.keys.removeAll(key);
+			delete this.data[key];
+		}
 
-        cell (key: string) : Cell{
-            return this.data[key];
-        }
+		cell (key: string) : Cell{
+			return this.data[key];
+		}
 
-        get (key: string) : any;
-        get (key: string, caller : Base, onchange : (newvalue, oldvalue) => void, supressInitialEvent?: bool): any;
-        get (key: string, caller?: Base, onchange?: (newvalue, oldvalue) => void, supressInitialEvent?: bool): any {
-            if (!this.has(key))
-                throw new Error("Value for '" + key + "' is not yet defined!")
+		get (key: string) : any;
+		get (key: string, caller : Base, onchange : (newvalue, oldvalue) => void, supressInitialEvent?: bool): any;
+		get (key: string, caller?: Base, onchange?: (newvalue, oldvalue) => void, supressInitialEvent?: bool): any {
+			if (!this.has(key))
+				throw new Error("Value for '" + key + "' is not yet defined!")
 
-            return (<Cell>this.data[key]).get(caller, onchange, supressInitialEvent);
-        }
+			return (<Cell>this.data[key]).get(caller, onchange, supressInitialEvent);
+		}
 
-        has (key: string): bool {
-            return key in this.data;
-        }
+		has (key: string): bool {
+			return key in this.data;
+		}
 
-        replaySets(handler: (key:string, value:any)=> void) {
-            for(var key in this.data) //TODO: use this.keys, to preserve order
-                handler.call(key, this.get(key));
-        }
+		replaySets(handler: (key:string, value:any)=> void) {
+			for(var key in this.data) //TODO: use this.keys, to preserve order
+				handler.call(key, this.get(key));
+		}
 
-        onSet (caller: Base, callback: (key : string,  newvalue : any, oldvalue : any) => void) {
-            return this.on('set', caller, callback);
-        }
+		onSet (caller: Base, callback: (key : string,  newvalue : any, oldvalue : any) => void) {
+			return this.on('set', caller, callback);
+		}
 
-        onRemove(caller: Base, callback: (key: string, oldvalue: any) => void ) {
-            return this.on('remove', caller, callback);
-        }
+		onRemove(caller: Base, callback: (key: string, oldvalue: any) => void ) {
+			return this.on('remove', caller, callback);
+		}
 
-        toObject (recurse?: bool): Object { //TODO: implement recurse
-            var res = {};
-            for(var key in this.data)
-                res[key] = this.get(key);
-            return res;
-        }
+		toObject (recurse?: bool): Object { //TODO: implement recurse
+			var res = {};
+			for(var key in this.data)
+				res[key] = this.get(key);
+			return res;
+		}
 
 		toAST(): Object { //TODO: implement recurse
 			var res = {
@@ -82,12 +82,12 @@ module NOA{
 			return res;
 		}
 
-        free () {
-            for(var key in this.data)
-                (<Cell>this.data[key]).die().free();
+		free () {
+			for(var key in this.data)
+				(<Cell>this.data[key]).die().free();
 
-            this.keys.free();
-            super.free();
-        }
-    }
+			this.keys.free();
+			super.free();
+		}
+	}
 }
