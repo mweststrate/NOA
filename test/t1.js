7bf96445ef3e70dfbc6a364e059600c55ba60229
Michel Weststrate <mweststrate@gmail.com>
2013-05-22 16:50:37 +0200

    improved record tests

--- test/t1.js
+++ test/t1.js
@@ -671,7 +671,7 @@ exports.testavg = function(test) {
 
 //TODO: test exception
 
-exports.test7 = function(test) {
+exports.record1 = function(test) {
 
     var o = new NOA.Record().live();
     o.put("a", 2);
@@ -684,24 +684,9 @@ exports.test7 = function(test) {
     test.deepEqual(o.keys.toJSON(), ["a","c"]);
 
     o.put("b", 2);
-/* TODO:
-    var f = Lang.let(
-        o,
-        "this",
-        LangUtils.withValues([], function() { //TODO: what is withValues without values? watchFunction?
-            return Lang.dot("this","a") + Lang.dot("this","b");
-        }
-    );
-
-    o.put("c", f);
-
-    test.equal(o.get("c"), '3');
-*/
     o.put("a", 10);
     o.put("b", 7);
-//    test.equal(o.get("c"), '17');
 
-    //f.live().die();
     o.die();
 
     test.equal(NOA.List.count, 0);
@@ -711,6 +696,46 @@ exports.test7 = function(test) {
 
     test.done();
 
+};
+
+
+exports.record2 = function(test) {
+
+    var o = new NOA.Record().live();
+    o.put("a", 2);
+    o.put("b", 2);
+    o.put("c", 5);
+    o.put("a", 1);
+    o.remove("b");
+
+    test.equal(JSON.stringify(o.toJSON()), '{"a":1,"c":5}');
+    test.deepEqual(o.keys.toJSON(), ["a","c"]);
+
+    o.put("b", 2);
+    var Lang = NOA.Lang;
+    var f = Lang.let(
+        o,
+        "this",
+        NOA.LangUtils.withValues([], function() { //TODO: what is withValues without values? watchFunction?
+            return Lang.dot("this","a") + Lang.dot("this","b");
+        })
+    );
+
+    o.put("c", f);
+
+    test.equal(o.get("c"), '3');
+    o.put("a", 10);
+    o.put("b", 7);
+    test.equal(o.get("c"), '17');
+
+    o.die();
+
+    test.equal(NOA.List.count, 0);
+    test.equal(NOA.Record.count, 0);
+    test.equal(NOA.Variable.count, 0);
+    test.equal(NOA.Constant.count, 0);
+
+    test.done();
 };
 
     if ((typeof(module) !== "undefined" && !module.parent) || typeof(window) !== "undefined")
