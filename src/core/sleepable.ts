66bb89c0a47d814bc4885a1b8e1efda9e0cc52e5
mweststrate <michel@mweststrate.nl>
2013-05-29 11:54:04 +0200

    Introduced the idea of sleepable

---
+++ src/core/sleepable.ts
@@ -0,0 +1,40 @@
+module NOA {
+/*
+export class Sleepable extends Base {
+
+	sleeping: bool = true;
+
+	constructor() {
+		//should disable event firing on base class somehow
+	}
+
+	wakeup(caller: Sleepable, context: any) {
+		Util.assert(this.noabase.refcount > 0);
+
+		var wassleeping = this.sleeping;
+
+		if (this.sleeping) {
+			//propagetes event automatically
+			this.getSleepingChildren().forEach(child => child.wakeup(this, context));
+
+			this.sleeping = false; //Note, events should not propagate as long as we are sleeping
+			
+			//fire wakeup event in general
+		}
+		else {
+			//fire events for caller specificely
+		}
+
+	}
+
+	getSleepingChildren(): Sleepable[] {
+		return null;
+	}
+
+	//returns event name + args, is fired, or fired for caller only 
+		getWakeupEvent(): any[] {
+
+		}
+}
+*/
+}
